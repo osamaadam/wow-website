f932bee1dd27eba1a3de344be3a810cc34cbc2e7
Osama Adam <osamaadam98@gmail.com>
2020-07-19 17:18:08 +0200

    removed sequelize models

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
+import { Sequelize } from "sequelize";
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+const authDB = new Sequelize(
+  "acore_auth",
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    dialect: "mysql",
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}..`);
