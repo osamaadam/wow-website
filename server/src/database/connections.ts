0b9c76e5d44bda85518327b2757dfc22462e5899
Osama Adam <osamaadam98@gmail.com>
2020-07-21 03:00:51 +0200

    This has to work

--- server/src/database/connections.ts
+++ server/src/database/connections.ts
@@ -2,9 +2,11 @@ import mysql from "mysql2";
 
 require("dotenv").config();
 
+const HOST = process.env.HOST || "0.0.0.0";
+
 export const authDB = mysql
   .createPool({
-    host: "host.docker.internal",
+    host: HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: "acore_auth",
