53c22351474eceb7eadfd2dd9a20e070e8f185c5
Osama Adam <osamaadam98@gmail.com>
2020-07-21 02:43:13 +0200

    changed mysql local ip

--- server/src/database/connections.ts
+++ server/src/database/connections.ts
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 export const authDB = mysql
   .createPool({
-    host: "0.0.0.0",
+    host: "127.0.0.1",
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: "acore_auth",
