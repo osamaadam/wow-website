8c14d2802a5b6d22abe394c365521b74e149631a
Osama Adam <osamaadam98@gmail.com>
2020-07-21 02:51:55 +0200

    trying

--- server/src/database/connections.ts
+++ server/src/database/connections.ts
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 export const authDB = mysql
   .createPool({
-    host: "127.0.0.1",
+    host: "host.docker.internal",
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: "acore_auth",
